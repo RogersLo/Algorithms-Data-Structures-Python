1c050fc5af6c6684ee6095191f258893f3ccb4c1
rogers <rogerslo0704@gmail.com>
2023-10-29 18:19:06 +0800

    complete 3-linkedlist practice.py

--- practice_3-doubly_linked_list.py
+++ practice_3-doubly_linked_list.py
@@ -17,7 +17,7 @@ class DoublyLinkedList:
             tmp = tmp.next
             if tmp:
                 print(" <-> ", end="")
-        
+        print()
                      
     def append(self, value):
         new_node = Node(value)
@@ -53,8 +53,8 @@ class DoublyLinkedList:
         tmp = self.head
         self.head = self.head.next
         self.head.prev = None
-        # tmp = None
-        return
+        tmp.next = None # tmp.next要 = None的原因是要讓原本頭部節點對下一個節點的連結斷掉
+        return tmp # return tmp就是要return被刪除的是誰
         
     def pop(self):
         if self.head == None:
@@ -69,7 +69,7 @@ class DoublyLinkedList:
         self.tail = tmp.prev
         self.tail.next = None
         tmp.prev = None
-        return
+        return tmp
         
         
         
@@ -79,11 +79,17 @@ if __name__ == "__main__":
     LinkedList.append(1)
     LinkedList.append(2)
     LinkedList.append(3)
-    LinkedList.prepend(5)
-    LinkedList.prepend(77)
-    LinkedList.pop()
+    LinkedList.append(4)
+    LinkedList.append(5)
 
-    # LinkedList.pop_first()
+    # LinkedList.prepend(5)
+    # LinkedList.prepend(77)
+    LinkedList.print_linkedlist()
+    print("==========")
+    LinkedList.pop_first()
+    LinkedList.print_linkedlist()
+    print("==========")
+    LinkedList.pop()
     LinkedList.print_linkedlist()
 
     
--- practice_3-linked_list.py
+++ practice_3-linked_list.py
@@ -17,6 +17,7 @@ class Linkedlist:
             tmp = tmp.next
             if tmp:
                 print("->", end=" ")  
+        print()
                 
     def append(self, value):
         new_node = Node(value)
@@ -51,14 +52,13 @@ class Linkedlist:
         # 鍊表有兩個元素及以上
         tmp = self.head
         self.head = self.head.next
-        tmp.next = None
-        return tmp
+        tmp = None
+        # return tmp
     
     def pop(self):
         # 跟pop_first一樣，一共分三種case: 鍊表為空，鍊表只有一個元素，以及第三種情況 (大於等於兩個元素)
         if self.head == None:
             return
-        
         if self.head.next == None:
             self.head = None
             self.tail = None
@@ -71,7 +71,31 @@ class Linkedlist:
         tmp.next = None
         self.tail = tmp
         return tmp
-           
+
+    def reverse(self):
+        # o(N)
+        # 如果是空元素或者只有一個元素，則不用反轉
+        if self.head == None:
+            return
+        if self.head.next == None:
+            return self.head
+        
+        # 兩個以上的元素才需要反轉
+        prev = None
+        curr = self.head
+        after = self.head.next
+
+        # 這裡要思考的是指針的移動而不是節點，例如curr.next = prev指的就是指針反轉後應該指向prev
+        while after:
+            # 先斷開current連結指向previous
+            curr.next = prev 
+            # 以下將三個位置指針分別往前移動
+            prev = curr
+            curr = after
+            after = after.next
+        
+        curr.next = prev
+        self.head, self.tail = self.tail, self.head
                    
 if __name__ == "__main__":
     print("\n====== Below is append area ======")
@@ -86,9 +110,12 @@ if __name__ == "__main__":
     Linkedlist.print_linkedlist()
     print("\n====== Below doing pop_first ======")
     Linkedlist.pop_first()
-    Linkedlist.pop_first()
     Linkedlist.print_linkedlist()
     print("\n====== Below doing pop ======")
     Linkedlist.pop()
-    Linkedlist.pop()
     Linkedlist.print_linkedlist()
+    print("\n====== Below doing reverse ======")
+    Linkedlist.reverse()
+    Linkedlist.print_linkedlist()
+
+
